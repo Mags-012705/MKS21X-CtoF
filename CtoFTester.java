12a5d9c3f1ab955d6b68b5bfe635c3f6baccea9c
Maggie Huang <mhuang30@stuy.edu>
2021-09-24 00:33:56 -0400

    Test Commit

---
+++ CtoFTester.java
@@ -0,0 +1,22 @@
+public class CtoFTester{
+  public static void main(String[] args){
+    System.out.println("Hello world!");
+  }
+
+  /*
+  What should the functions have as parameters?
+    The functions should have the temperature to be converted into the other unit as parameters.
+    For celsiusToFahrenheit, celsius should be the parameter.
+    For fahrenheitToCelsius, fahrenheit should be the parameter.
+
+  What type should they be?
+    They could be either integers or doubles. I prefer to use doubles here so I don't have to converted
+    later for division and multiplication. Plus, temperatures can have decimals so it's easier that way.
+
+  What should the functions return?
+    The functions should return the converted temperature.
+    For celsiusToFahrenheit, it should return the fahrenheit equivilant of the inputted celsius.
+    For fahrenheitToCelsius, it should return the celsius equivilant of the inputted fahrenheit.
+
+  */
+}
